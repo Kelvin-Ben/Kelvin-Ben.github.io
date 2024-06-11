df57a3c24bbd43ceaa61de28d239c379a53f16e3
Kelvin-Ben <kevoben14@gmail.com>
2024-06-11 18:14:28 +0300

    Add attention component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Attention from "./components/Attention";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-customBlue flex justify-center">
+      <Attention />
+    </div>
+  );
+};
+
+export default App;
