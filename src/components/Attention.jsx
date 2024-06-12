1675e9cd5243fb99579656cff6d012c626d85d80
Kelvin-Ben <kevoben14@gmail.com>
2024-06-12 16:52:22 +0300

    Improve attention page

--- src/components/Attention.jsx
+++ src/components/Attention.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Attention = () => {
   return (
-    <div className="w-[30%] h-[50%] m-auto border flex flex-col items-center justify-center" style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.8'}}>
-      <h1 className="uppercase text-3xl">we're under construction</h1>
+    <div className="w-[30%] h-[50%] m-auto border flex flex-col items-center text-center justify-center lg:w-[80%] md:w-[80%] sm:w-[80%] sm:text-2xl" style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.8'}}>
+      <h1 className="uppercase text-3xl border border-red-600">we're under construction</h1>
       <h2 className="font-bellefair">
         Our website is coming soon. Stay tuned for something interesting!
       </h2>
