7e55e4883c6f835bce8a1a642cd62283ff7ed039
Kelvin-Ben <kevoben14@gmail.com>
2024-07-27 21:32:59 +0300

    Create a routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 
-import Root from "./components/Root";
-import Home from "./components/Home/Home";
-import Projects from "./components/Projects/Projects";
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
+import Projects from "./feature/Projects/Projects";
+import About from "./feature/About/About";
+import Contact from "./feature/Contact/Contact";
+import Home from "./feature/Home/Home";
+import { Layout } from "./components/Layout";
 
-const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<Root />}>
-  <Route path="/" element={<Home />} />
-  <Route path="/projects" element={<Projects />} />
-  <Route path="/about" element={<About />} />
-  <Route path="/contact" element={<Contact />} />
-</Route>))
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+    <Route path="/projects" element={<Projects />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+  </Route>))
 
 const App = () => {
   return (
