2c81bbe771c419900d510d06b40f8e5b19678541
Kelvin-Ben <kevoben14@gmail.com>
2024-07-28 13:44:20 +0300

    Add a dummy text in about section

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='my-[2rem]'>
+      <h1 className='font-sans text-[3rem] font-bold'>About me</h1>
+      <p className='text-[1.5rem]'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+        in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  )
+}
